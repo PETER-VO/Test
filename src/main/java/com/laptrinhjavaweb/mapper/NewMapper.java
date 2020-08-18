62a61f4129cb466ff0f4f04dc76ed16f07bac180
Fong-Vo <61008063+Fong-Vo@users.noreply.github.com>
2020-08-18 06:31:55 +0300

    init project

---
+++ src/main/java/com/laptrinhjavaweb/mapper/NewMapper.java
@@ -0,0 +1,35 @@
+package com.laptrinhjavaweb.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.laptrinhjavaweb.model.NewModel;
+
+public class NewMapper implements RowMapper<NewModel>{
+
+	@Override
+	public NewModel mapRow(ResultSet resultSet) {
+		try {
+			NewModel news = new NewModel();
+			news.setId(resultSet.getLong("id"));
+			news.setTitle(resultSet.getString("title"));
+			news.setThumbnail(resultSet.getString("thumbnail"));
+			news.setShortDescription(resultSet.getString("shortdescription"));
+			news.setContent(resultSet.getString("content"));
+			news.setCategoryId(resultSet.getLong("categoryid"));
+			news.setCreatedDate(resultSet.getTimestamp("createddate"));
+			if (resultSet.getTimestamp("modifieddate") != null) {
+				news.setModifiedDate(resultSet.getTimestamp("modifieddate"));
+			}
+			news.setCreatedBy(resultSet.getString("createdby"));
+			if (resultSet.getString("modifiedby") != null) {
+				news.setModifiedBy(resultSet.getString("modifiedby"));
+			}
+			return news;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+	}
+
+}
